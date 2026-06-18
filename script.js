fd1f7e4a0bfd6935fbabb7d2f885ee2f0964a7a0
Zazzix <81529822+Zazzix@users.noreply.github.com>
2026-06-18 15:11:13 +0200

    fix

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
 'use strict';
 
 function roll(dice) {
-    if (isNaN(dice)) {
-        return null;
+    const diceSides = ['d4', 'd6', 'd8', 'd10', 'd12', 'd16', 'd20'];
+
+    if (!diceSides.includes(dice)) {
+        return false;
     }
 
-    let max = Number.parseInt(dice);
-    let min = dice / dice;
-    
+    let max = parseInt(dice.slice(1));
+    let min = max / max;
+
+
     const diceRoll = Math.floor(Math.random() * (max - min + 1) + min);
 
     return diceRoll;
+
+
 }
+
 

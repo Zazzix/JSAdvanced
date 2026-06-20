46d7f7f866653d0ca07612d1b804453e37136655
Zazzix <81529822+Zazzix@users.noreply.github.com>
2026-06-20 21:28:16 +0200

    remove comments

--- birthday.js
+++ birthday.js
@@ -1,18 +1,6 @@
 'use strict';
 
 function birthYearCheck(date) {
-    /*
-    let birthday = new Date(date)
-    let birthYear = birthday.getFullYear();
-    let birthMonth = birthday.getMonth();
-    let birthDate = birthday.getDate();
-
-    let currentYear = new Date().getFullYear();
-    let currentMonth = new Date().getMonth();
-    let currentDate = new Date().getDate();
-
-    if (currentYear - birthYear > 14 && ());
-    */
     let birthday = new Date(date).setHours(0,0,0,0);
     let current = new Date().setHours(0,0,0,0);
     const daysGap = 5114

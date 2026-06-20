18f723832f04fceaf9b3ffdd4d0016f299cbc9d0
Zazzix <81529822+Zazzix@users.noreply.github.com>
2026-06-20 21:18:20 +0200

    fix1

--- birthday.js
+++ birthday.js
@@ -1,9 +1,26 @@
 'use strict';
 
 function birthYearCheck(date) {
+    /*
     let birthday = new Date(date)
     let birthYear = birthday.getFullYear();
-    let currentYear = new Date().getFullYear();
+    let birthMonth = birthday.getMonth();
+    let birthDate = birthday.getDate();
 
-    return currentYear - birthYear > 14;
+    let currentYear = new Date().getFullYear();
+    let currentMonth = new Date().getMonth();
+    let currentDate = new Date().getDate();
+
+    if (currentYear - birthYear > 14 && ());
+    */
+    let birthday = new Date(date).setHours(0,0,0,0);
+    let current = new Date().setHours(0,0,0,0);
+    const daysGap = 5114
+
+
+    const days =  (current - birthday) / (1000 * 60 * 60 * 24);
+
+    return days < daysGap
 }
+
+console.log(birthYearCheck('2012-06-20'));

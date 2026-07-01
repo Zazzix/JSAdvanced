3716b5cba16b2d13c3d01d55ae552b2a21f1a93e
Zazzix <81529822+Zazzix@users.noreply.github.com>
2026-07-01 14:11:22 +0200

    solve solid task

---
+++ solid.js
@@ -0,0 +1,47 @@
+'use strict';
+
+class Billing {
+    amount;
+
+    constructor(amount) {
+        this.amount = amount;
+    }
+
+    calculateTotal() {
+
+    }
+}
+
+class fixBilling extends Billing {
+    constructor(amount) {
+        super(amount);
+    }
+
+    calculateTotal() {
+        return this.amount
+    }
+}
+
+class hourBilling extends Billing {
+    hours;
+    constructor(amount, hours) {
+        super(amount);
+        this.hours = hours;
+    }
+
+    calculateTotal() {
+        return this.amount * this.hours;
+    }
+}
+
+class itemBilling extends Billing {
+    items;
+    constructor(amount, items) {
+        super(amount);
+        this.items = items;
+    }
+
+    calculateTotal() {
+        return this.amount * this.items;
+    }
+}
